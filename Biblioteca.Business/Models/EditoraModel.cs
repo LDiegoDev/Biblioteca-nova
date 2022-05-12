76e2c55b4b71fc6a75fd0873b20b7293c13782e0
Diego Araújo <Ldiegoaraujo@gmail.com>
2022-05-11 22:47:53 -0300

    Criação da model Editora.

---
+++ Biblioteca.Business/Models/EditoraModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblioteca.Business.Models
+{
+    public class EditoraModel : EntityModel
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        //    public TipoEditora TipoEditora { get; set; }
+        //   public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        //   public IEnumerable<Livro> Livros { get; set; }
+    }
+}
---
+++ Biblioteca.Business/Models/EntityModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblioteca.Business.Models
+{
+    public abstract class EntityModel
+    {
+        public Guid Id { get; set; }
+
+        protected EntityModel()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
